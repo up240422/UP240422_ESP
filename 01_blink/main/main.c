73d004f8c3f3ad850007ec62f3cdb5a6855c74d2
up240422 <up240422@alumnos.upa.edu.mx>
2025-10-07 11:33:56 -0600

    funcion sos terminada

--- 01_blink/main/main.c
+++ 01_blink/main/main.c
@@ -2,13 +2,45 @@
 #include "freertos/FreeRTOS.h" // Esta librería es necesaria para usar FreeRTOS 
 #include "freertos/task.h" // Esta librería es necesaria para usar las tareas de FreeRTOS
 
-#define LED GPIO_NUM_27 // Definimos el pin donde está conectado el LED
+#define LED GPIO_NUM_26 // Definimos el pin donde está conectado el LED
 
 void punto(){
     gpio_set_level(LED, 1); // Encendemos el LED
     vTaskDelay(pdMS_TO_TICKS(200)); // Esperamos 1 segundo
     gpio_set_level(LED, 0); // Apagamos el LED
     vTaskDelay(pdMS_TO_TICKS(200)); // Esperamos 1 segundo
+}
+
+void raya(){
+    gpio_set_level(LED, 1); // Encendemos el LED
+    vTaskDelay(pdMS_TO_TICKS(500)); // Esperamos 1 segundo
+    gpio_set_level(LED, 0); // Apagamos el LED
+    vTaskDelay(pdMS_TO_TICKS(500)); // Esperamos 1 segundo
+}
+
+void sos(){
+    for(int i =0; i < 3; i++){
+        punto();
+    }
+    for (int i = 0; i < 3; i++){
+        raya();
+    }
+     for(int i =0; i < 3; i++){
+        punto();
+    }
+}
+
+void app_main(void) // Función principal del programa
+{
+
+    gpio_reset_pin(LED); // Reseteamos el pin del LED
+    gpio_set_direction(LED, GPIO_MODE_OUTPUT); // Configuramos el pin del LED como salida
+
+    while (1) // Bucle infinito
+    {
+        sos();
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
 }
 
 
@@ -59,56 +91,4 @@ void punto(){
 
 
 
-
-
-
-
-
-void app_main(void) // Función principal del programa
-{
-
-    gpio_reset_pin(LED); // Reseteamos el pin del LED
-    gpio_set_direction(LED, GPIO_MODE_OUTPUT); // Configuramos el pin del LED como salida
-
-    while (1) // Bucle infinito
-    {
-        gpio_set_level(LED, 1); // Encendemos el LED
-        vTaskDelay(pdMS_TO_TICKS(200)); // Esperamos 1 segundo
-        gpio_set_level(LED, 0); // Apagamos el LED
-        vTaskDelay(pdMS_TO_TICKS(200)); // Esperamos 1 segundo
-        gpio_set_level(LED, 1); // Encendemos el LED
-        vTaskDelay(pdMS_TO_TICKS(200)); // Esperamos 1 segundo
-        gpio_set_level(LED, 0); // Apagamos el LED
-        vTaskDelay(pdMS_TO_TICKS(200)); // Esperamos 1 segundo
-        gpio_set_level(LED, 1); // Encendemos el LED
-        vTaskDelay(pdMS_TO_TICKS(200)); // Esperamos 1 segundo
-        gpio_set_level(LED, 0); // Apagamos el LED
-        //salto a la letra O
-        vTaskDelay(pdMS_TO_TICKS(1000)); // Esperamos 1 segundo
-        gpio_set_level(LED, 1); // Encendemos el LED
-        vTaskDelay(pdMS_TO_TICKS(500)); // Esperamos 1 segundo
-        gpio_set_level(LED, 0); // Encendemos el LED
-        vTaskDelay(pdMS_TO_TICKS(500)); // Esperamos 1 segundo
-        gpio_set_level(LED, 1); // Encendemos el LED
-        vTaskDelay(pdMS_TO_TICKS(500)); // Esperamos 1 segundo
-        gpio_set_level(LED, 0); // Encendemos el LED
-        vTaskDelay(pdMS_TO_TICKS(500)); // Esperamos 1 segundo
-        gpio_set_level(LED, 1); // Encendemos el LED
-        vTaskDelay(pdMS_TO_TICKS(500)); // Esperamos 1 segundo
-        gpio_set_level(LED, 0); // Encendemos el LED
-        vTaskDelay(pdMS_TO_TICKS(1000)); // Esperamos 1 segundo
-        //salto a la letro S
-        gpio_set_level(LED, 1); // Encendemos el LED
-        vTaskDelay(pdMS_TO_TICKS(200)); // Esperamos 1 segundo
-        gpio_set_level(LED, 0); // Apagamos el LED
-        vTaskDelay(pdMS_TO_TICKS(200)); // Esperamos 1 segundo
-        gpio_set_level(LED, 1); // Encendemos el LED
-        vTaskDelay(pdMS_TO_TICKS(200)); // Esperamos 1 segundo
-        gpio_set_level(LED, 0); // Apagamos el LED
-        vTaskDelay(pdMS_TO_TICKS(200)); // Esperamos 1 segundo
-        gpio_set_level(LED, 1); // Encendemos el LED
-        vTaskDelay(pdMS_TO_TICKS(200)); // Esperamos 1 segundo
-        gpio_set_level(LED, 0); // Apagamos el LED
-        vTaskDelay(pdMS_TO_TICKS(5000)); // Esperamos 1 segundo
-    }
 
